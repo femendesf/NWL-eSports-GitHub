5a812b210f1ba15ed50468b317cce2ab19082f0d
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2022-09-15 15:36:46 -0300

    node configuration
    Node configurado para trabalhar com TypeScript

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express()
+
+app.get('/ads', (request, response) => {
+    return response.json([
+        { id: 1, name: 'Anúncio 1'},
+        { id: 2, name: 'Anúncio 2'},
+        { id: 3, name: 'Anúncio 3'},
+        { id: 4, name: 'Anúncio 4'},
+        { id: 5, name: 'Anúncio 5'},
+    ])
+}) //request é a requisição que nós vamos pegar, no caso do anuncio, podemos pegar qual é o jogo, o tempo jogado e outros. E o response serva para devolver uma resposta
+   
+
+app.listen(3333)
