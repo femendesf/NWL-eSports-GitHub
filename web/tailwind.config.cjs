a8fca09b2751744941b26aa384a23e801a5919ae
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2022-09-16 21:24:29 -0300

    Layout web finalizado
    Definido a estilização dos componentes, adicionamos o conteudo e usamos a biblioteca phosphor para importar icones no react

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,10 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.tsx'
+    // Os arquivos de conteudo esperado pelo Tailwind
+    './src/**/*.tsx',
+    './index.html',
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      fontFamily:{
+        sans: ['Inter', 'sans-serif']
+      },
+
+      backgroundImage:{
+        galaxy: "url('./background-galaxy.png')", // Imagem fundo página Web
+        'nlw-gradient':'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradient':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
   },
   plugins: [],
 }
