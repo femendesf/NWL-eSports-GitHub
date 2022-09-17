64680018dc9c14391504be0cf547539c7845e7b6
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2022-09-16 23:10:26 -0300

    update file src e asset

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,37 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-
-interface ButtonProps{
-  title: string;
-}
-
-function Button(props: ButtonProps){
-  return(
-    <TouchableOpacity>
-      <Text>
-        {props.title}
-      </Text>
-    </TouchableOpacity>
-  )
-}
+import { Background } from "./src/Components/Background";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        NWL eSports
-      </Text>
-      <Button title="Clique"/>
-      <StatusBar style="auto" />
-    </View>
+    <Background>
+      
+    </Background>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFF',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ mobile/src/Components/Background/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ImageBackground} from 'react-native';
+
+import { styles } from './styles';
+
+interface Props {
+    children:React.ReactNode;
+}
+
+
+export function Background({children}: Props) {
+  return(
+    <ImageBackground style={styles.container}>
+      {children}
+    </ImageBackground>
+  )
+
+}
