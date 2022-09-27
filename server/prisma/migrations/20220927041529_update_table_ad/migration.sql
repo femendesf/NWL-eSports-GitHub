bbc96940045256698f69fa5413bed2f581c4ac6c
Felipe Mendes Fonseca <felipe.cwb13@gmail.com>
2022-09-27 02:55:56 -0300

    update server
    todas as rotas criadas, banco de dados finalizado e tudo configurado

--- server/prisma/migrations/20220927041529_update_table_ad/migration.sql
+++ server/prisma/migrations/20220927041529_update_table_ad/migration.sql
@@ -1,5 +1,6 @@
--- CreateTable
-CREATE TABLE "Ad" (
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Ad" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "gameId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -12,3 +13,8 @@ CREATE TABLE "Ad" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT "Ad_gameId_fkey" FOREIGN KEY ("gameId") REFERENCES "Game" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
+INSERT INTO "new_Ad" ("createdAt", "discord", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearsPlaying") SELECT "createdAt", "discord", "gameId", "hourEnd", "hourStart", "id", "name", "useVoiceChannel", "weekDays", "yearsPlaying" FROM "Ad";
+DROP TABLE "Ad";
+ALTER TABLE "new_Ad" RENAME TO "Ad";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,119 +1,63 @@
-/* HTTP methods / API RESTful / HTTP Codes
-
-**************************************** HTTP METHODS ******************************************************************
-GET: Fazer uma leitura no nosso back-end.
-POST: Criar algo no back-end.
-PUT: Editar um entidade por completo, no caso um perfil, que pode ser editado varios campos ao mesmo tempo.
-PATCH: Editar uma informação especifica dentro de uma entidade.
-DETELE: deletar.
-...
-************************************************************************************************************************
-
-
-********************************************** HTTP CODES **************************************************************
-
-O HTTP Codes vai mostrar se a resposta que estamos recebendo, é uma resposta válida, qual que é o tipo de resposta que estamos recebendo do nosso back-end.
-
-Os principais HTTP CODES são os que começam com:
-'.status()':
-2 - Sucesso,
-3 - Redirecionamento,
-4 - Erros gerado pela aplicação,
-5 - Erros inesperados, não foi gerado por algum código especifico.
-....
-*************************************************************************************************************************
-
-*/
-
-/* Tipos de parâmetros da comuniação back-end e front-end:
- 
-  Query: são os parâmetros que vem da URL através do ponto de interrogação,
-    Exemplo: localhost:3333/ads?page=2&sort=title
-    - Vai gravar o estado da página, no caso se selecionamos um filtro, qual pagina estamos, etc...
-    - Nunca vai passar informação sensivel, senhas por exemplo.
-    - Os parâmetros sempre são nomeados, por exemplo 'page', 'sorte'.
-
- * Route: também são os parametros da internet
-    - Não são nomedos.
-    -  Identificador direto na URL, por exemplo: localhost:3333/post/como-criar-anuncio
-
-
- * Body:
-    - Para enviar várias informações em uma unica requisição. Geralmente para envio de formulario.
-    - Não fica na URL, fica escondido na requisição.
-    - Recomendado para informações sensiveis, como cadastrar ou mudar senha
- */
-
-/* Async/Await:
-    Usamos para quando queremos que primeiro seja executado uma instrução.
-    Adicionar 'async' na função que esta por volta da instrução que queremos que seja executada, e adicionamos 'await' antes da instrução.
-*/
-
-
-import express, { query, request, response } from "express";
-import {PrismaClient} from '@prisma/client'
-import { convertHourStringToMinutes } from "./utils/convert-hour-strinh-to-minutes";
+import express, { request, response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { convertHourStringToMinutes } from "./util/convert-hour-to-string";
+import { convertMinutesToHourString } from "./util/convert-minutes-to-hour-string";
+import cors from 'cors'
 
 const app = express()
 const prisma = new PrismaClient({
+    log: ['query']
+})
 
-    log:['query']
-});
 
-app.use(express.json()) // Por padrão o Express não entende informação no formato de JSON, com esse código esta fazendo com que ele entenda :).
+app.use(express.json()) // Linha para express entender requisição JSON
+app.use(cors())
 
-app.post('/games/:id/ads', async (request, response) => {
 
-    /*
-        Body / Request:
-        Vai ser usado o Body porque esta sendo passado várias informações, e algumas pode ser sensiveis.
-    */
-    const gameId = request.params.id;
-    const body: any = request.body;
+app.post("/game/:id/ads", async(request, response) => {
+    
+    const gameId = request.params.id
+    const body: any = request.body
 
     const ad = await prisma.ad.create({
-        data: {
+
+        data:{
             gameId,
             name: body.name,
             yearsPlaying: body.yearsPlaying,
             discord: body.discord,
-            weekDays: body.weekDays.join(","),
+            weekDays: body.weekDays.join(','),
             hourStart: convertHourStringToMinutes(body.hourStart),
-            hourEnd: convertHourStringToMinutes(body.hourEnd),
-            useVoiceChannel: body.useVoiceChannel,
+            hourEnd:convertHourStringToMinutes(body.hourEnd),
+            useVoiceChannel: body.useVoiceChannel
         }
     })
 
     return response.status(201).json(ad)
-
-})// Rota para criar anuncios.
-//Por mais que o endereço '/ads' possa ser o mesmo da outra rota, vai ser diferente por conta do método. Como usamos métodos diferentes, não precisamos criar um nome diferente na rota.
+}) // Rota para criar anuncios.
 
 
-app.get('/games', async (request, response) => {
-    
-    const games = await prisma.game.findMany({
+app.get("/games", async(request, response) => {
+
+    const games = await prisma.game.findMany({ // Mostra a quantidade de anuncios no game
         include:{
             _count:{
                 select:{
-                    ads:true
+                    ads: true
                 }
             }
         }
-    }) 
-    /*Instrução para encontrar todos os jogos na tabela do banco de dados.
-    Pode passar um objeto de configuração dentro do nosso findMany, no caso estamos usando um pra mostrar a contagem dos anuncios*/ 
+    })
 
     return response.json(games)
+}) // Rota para listar os games.
 
-});//Rota para listar os games.
 
+app.get("/games/:id/ads", async(request, response) => {
 
-app.get('/games/:id/ads', async(request, response) => { // Rota para acessar os anuncios do game.
+    const gameId = request.params.id
 
-    const gameId = request.params.id // Retorna o parametro id que esta vindo da URL
-    
-    const ads = await prisma.ad.findMany({ // Intrução retorna os anuncios do game especifico
+    const ads = await prisma.ad.findMany({
         select:{
             id: true,
             name: true,
@@ -121,29 +65,29 @@ app.get('/games/:id/ads', async(request, response) => { // Rota para acessar os 
             useVoiceChannel: true,
             yearsPlaying: true,
             hourStart: true,
-            hourEnd: true
+            hourEnd: true,
         },
-
         where:{
             gameId
         },
-
         orderBy:{
-            createdAt: 'desc'
+            createdAt:"desc"
         }
     })
 
-    return response.json(ads.map(ad => {
-        return {
+    return response.json(ads.map(ad =>{
+        return{
             ...ad,
-            weekDays: ad.weekDays.split(',') //Formatação do retorno dos dias da semana
+            weekDays: ad.weekDays.split(','),
+            hourStart: convertMinutesToHourString(ad.hourStart),
+            hourEnd: convertMinutesToHourString(ad.hourEnd)
         }
     }))
-}) //request é a requisição que vamos pegar. No caso do anuncio podemos pegar qual é o jogo, o tempo jogado e outros. E o response serve para devolver uma resposta
+}) // Rota para listas os anuncios por game.
 
 
-app.get('/add/:id/discord', async (request, response) => {
-    const AdID = request.params.id
+app.get("/ads/:id/discord", async(request, response) => {
+    const adId = request.params.id
 
     const ad = await prisma.ad.findUniqueOrThrow({
         select:{
@@ -151,13 +95,14 @@ app.get('/add/:id/discord', async (request, response) => {
         },
 
         where:{
-            id: AdID
+            id: adId
         }
     })
+
     return response.json({
         discord: ad.discord
     })
-}) // Rota para buscar o Discord pelo ID do anúncio.
+}) // Rota para buscar o discord do anuncio
 
 
 app.listen(3333)
